13df82cd259932784ab622d900eb05c1ca00b903
Mike Silva <mike.a.silva@gmail.com>
2014-07-03 21:37:11 -0400

    Updates to Metro Worker Productivity
    I broke out the BEA API Functions into their own file.  I added
    metrolist and output csv files.

---
+++ BEA API Functions.R
@@ -0,0 +1,61 @@
+# ============================================================================
+# BEA API Functions.R
+# ============================================================================
+# Written by: Mike Silva
+# ============================================================================
+# These function serves as a wrapper for the BEA's API.  You pass in your API 
+# key, a vector of KeyCodes and GeoFIPS and the function returns a matrix with
+# all the data.  If you run it through the cleanData function it will scrub it
+# and return a data frame.
+#
+# For more information please refer to the BEA API documentation at:
+# http://www.bea.gov/api/_pdf/bea_web_service_api_user_guide.pdf
+# ============================================================================
+
+beaAPI <- function(api.key, key.code.vector, geo.fips.vector){
+  library(RCurl) 
+  library(rjson)
+  
+  data.matrix <- matrix(ncol=7, nrow=0)
+  
+  api.url <- 'http://www.bea.gov/api/data/'
+  
+  key.codes <- length(key.code.vector)
+  geo.fipss <- length(geo.fips.vector)
+  total <- key.codes * geo.fipss
+  pb.i <- 0
+  pb <- txtProgressBar(min = pb.i, max = total, style = 3)
+  
+  for(key.code in key.code.vector){
+    for(geo.fips in geo.fips.vector){
+      req.uri <- paste0(api.url,
+                        '?&UserID=',api.key,
+                        '&KeyCode=',key.code,
+                        '&GeoFIPS=',geo.fips,
+                        '&method=GetData&datasetname=RegionalData&ResultFormat=JSON&'
+      )
+      json <- fromJSON(getURL(req.uri))
+      for(new.row in json$BEAAPI$Results$Data){
+        data.matrix <- rbind(data.matrix, new.row)
+      }
+      pb.i <- pb.i + 1
+      setTxtProgressBar(pb, pb.i)
+    }    
+  }
+  return(data.matrix)
+}
+
+cleanData <- function(df, data.value){
+  # Keep only the 3 columns we are interested in
+  keep <- c("GeoFips", "TimePeriod", 'DataValue')
+  df <- df[keep]
+  # Remove any duplicates
+  df <- df[!duplicated(df),]
+  # Change Variable Types
+  df$TimePeriod <- as.numeric(df$TimePeriod)
+  df$DataValue <- as.numeric(df$DataValue)
+  #Rename the columns
+  names(df) <- c('msa.fips', 'year', data.value)
+    
+  return(df)
+}
--- Metro Worker Productivity.R
+++ Metro Worker Productivity.R
@@ -1,67 +1,42 @@
-metrolist <- read.csv('metrolist.csv', header=F)
-names(metrolist) <- c('state.fips','state.name','msa.fips','msa.name')
+# ============================================================================
+# Metro Worker Productivity.R
+# ============================================================================
+# Written by: Mike Silva
+# ============================================================================
+# This script constructs a Metro Worker Productivity data set using the BEA's 
+# regional estimates as distributed by their API.
+# ============================================================================
 
-beaAPI <- function(api.key, key.code.vector, geo.fips.vector){
-  library(RCurl) 
-  library(rjson)
-  
-  data.matrix <- matrix(ncol=7, nrow=0)
-  
-  api.url <- 'http://www.bea.gov/api/data/'
-  
-  key.codes <- length(key.code.vector)
-  geo.fipss <- length(geo.fips.vector)
-  total <- key.codes * geo.fipss
-  pb.i <- 0
-  pb <- txtProgressBar(min = pb.i, max = total, style = 3)
-  
-  for(key.code in key.code.vector){
-    for(geo.fips in geo.fips.vector){
-      req.uri <- paste0(api.url,
-                        '?&UserID=',api.key,
-                        '&KeyCode=',key.code,
-                        '&GeoFIPS=',geo.fips,
-                        '&method=GetData&datasetname=RegionalData&ResultFormat=JSON&'
-                        )
-      json <- fromJSON(getURL(req.uri))
-      for(new.row in json$BEAAPI$Results$Data){
-        data.matrix <- rbind(data.matrix, new.row)
-      }
-      pb.i <- pb.i + 1
-      setTxtProgressBar(pb, pb.i)
-    }    
-  }
-  return(data.matrix)
-}
-
-cleanData <- function(df, data.value){
-  # Keep only the 3 columns we are interested in
-  keep <- c("GeoFips", "TimePeriod", 'DataValue')
-  df <- df[keep]
-  # Remove any duplicates
-  df <- df[!duplicated(df),]
-  # Change Variable Types
-  df$TimePeriod <- as.numeric(df$TimePeriod)
-  df$DataValue <- as.numeric(df$DataValue)
-  #Rename the columns
-  names(df) <- c('msa.fips', 'year', data.value)
-  
-  return(df)
-}
-
+# The following file assigns my BEA API key to the variable 'api.key'
 source('~/bea.api.key.R')
+# This file has the beaAPI and cleanData function
+source('~/BEA API Functions.R')
 
-key.code.vector <- c('EMP000_MI','RGDP_MP','POP_MI')
+# The following csv has the list of all the metros and their FIPS codes.  It 
+# was downloaded from http://www.bea.gov/regional/docs/msalist.cfm by 
+# selecting the MSA's by state and downloading the CSV.
+metrolist <- read.csv('metrolist.csv', header=FALSE)
+names(metrolist) <- c('state.fips','state.name','msa.fips','msa.name')
 geo.fips.vector <- metrolist$msa.fips
 
 # Download & Clean Total Employment Data
 employment <- beaAPI(api.key, 'EMP000_MI', geo.fips.vector)
-#employment <- cleanData(as.data.frame(employment), 'employment')
-
+employment <- cleanData(as.data.frame(employment), 'employment')
 
 # Download & Clean Real GDP data
 real.gdp <- beaAPI(api.key, 'RGDP_MP', geo.fips.vector)
-#real.gdp <- cleanData(as.data.frame(real.gdp), 'real.gdp')
+real.gdp <- cleanData(as.data.frame(real.gdp), 'real.gdp')
 
-productivity <- merge(cleanData(as.data.frame(real.gdp), 'real.gdp'), cleanData(as.data.frame(employment), 'employment'), by=c('msa.fips', 'year'), all.y=FALSE)
+# Create the productivity data frame
+productivity <- merge(real.gdp, employment, by=c('msa.fips', 'year'), all.y=FALSE)
 productivity$worker.productivity <- (productivity$real.gdp*1000000) / productivity$employment
+
+# Replace the lists
+productivity[] <- lapply(productivity, function(x) if(is.list(x)) unlist(x))
+employment[] <- lapply(employment, function(x) if(is.list(x)) unlist(x))
+real.gdp[] <- lapply(real.gdp, function(x) if(is.list(x)) unlist(x))
+
+# Save the data
+write.csv(productivity, 'Metro Worker Productivity.csv')
+write.csv(employment, 'Metro Total Employment.csv')
+write.csv(real.gdp, 'Metro Real GDP.csv')
